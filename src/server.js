d834abfa254711be3e692709e33f44f2ed0a7c4c
Rafael Machado Arantes <rafaelmachadoarantes@gmail.com>
2026-08-19 14:06:08 -0300

    fix: corrigindo rota para o arquivo index.html

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
-import {routerHealth, routerTranscricoes } from './router.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { routerHealth, routerTranscricoes } from './router.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Óla Rafael!');
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use('/healthz', routerHealth)
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api/transcricoes', routerTranscricoes)
+app.use('/healthz', routerHealth);
 
-app.listen(3000, () =>{
-    console.log('Servidor Iniciado')
+app.use('/api/transcricoes', routerTranscricoes);
+
+app.listen(3000, () => {
+    console.log('Servidor Iniciado');
 });
